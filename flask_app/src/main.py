bacc8fe0404039a19deee4a5f7a251e5688b0b85
Apratim Nath <nathapratim@gmail.com>
2021-02-09 13:34:44 +0530

    initialized Scheduler

--- flask_app/src/main.py
+++ flask_app/src/main.py
@@ -5,13 +5,9 @@ Created on Nov 28, 2020
 '''
 import all_api_endpoints
 import warnings
-import datetime
 
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    current_server_time= datetime.datetime.now()
-    print("The current server time is - ")
-    print(current_server_time)
     all_api_endpoints.app.run(threaded=True, port=8085, debug=True)
